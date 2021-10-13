26e5cece15b3789a97312a80d90ab49fa3a7bcd6
Bodzisz <kacpiw01pol@gmail.com>
2021-10-13 22:53:39 +0200

    Add ex 1-3 from 1st list

---
+++ List01/List01Functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bodzisz on 13.10.2021.
+//
+
+#include "List01Functions.h"
---
+++ List01/List01Functions.h
@@ -0,0 +1,15 @@
+//
+// Created by bodzisz on 13.10.2021.
+//
+
+#ifndef LIST01_LIST01FUNCTIONS_H
+#define LIST01_LIST01FUNCTIONS_H
+
+
+class List01Functions
+{
+
+};
+
+
+#endif //LIST01_LIST01FUNCTIONS_H
